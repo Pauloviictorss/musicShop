510cee5d8e892f50df5056c41f98c293f1a1af49
“Pauloviictorss” <paulovictorlvm@gmail.com>
2022-11-08 21:10:21 -0300

    Adicionando validação

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,19 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import User from "App/Models/User"
+
+export default class UsersController {
+    async store ({request}){
+        const dados = request.only(['email', 'password'])
+        return await User.create(dados)
+    }
+
+    async login ({request, auth}){
+        const email = request.input('email')
+        const password = request.input('password')
+
+        const token = await auth.use('api').attempt(email, password)
+
+        return token
+    }
+}
